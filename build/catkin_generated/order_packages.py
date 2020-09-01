d6755a52af87146a90c8b0adc6e31017c104df5a
Changhyeon Park <ch7170p@naver.com>
2020-09-01 11:52:57 +0900

    2020.09.01 MGPF-Hybrid A*, A* 수정

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/chp/path_planning/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/chp/path_planning/devel;/home/chp/psy_ws/devel;/opt/ros/kinetic".split(';') if "/home/chp/path_planning/devel;/home/chp/psy_ws/devel;/opt/ros/kinetic" != "" else []
